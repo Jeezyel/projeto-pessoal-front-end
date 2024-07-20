732b0f99290ba29f8c33c87aee0afdf5a26f23f6
Jeezyel <jeezyelpereira@gmail.com>
2024-07-20 00:32:25 -0300

    primeiro commit

---
+++ src/app/components/uplod/uplod.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uplod',
+  standalone: true,
+  imports: [],
+  templateUrl: './uplod.component.html',
+  styleUrl: './uplod.component.css'
+})
+export class UplodComponent {
+
+}
