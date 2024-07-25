0c0de43f4b7efd049ac11a92fc238356a1587c76
Jeezyel <jeezyelpereira@gmail.com>
2024-07-25 08:04:00 -0300

    criando a conecção com o beck

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,13 +5,17 @@ import { NgIf } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
+  declarations: [
     
+    
+  ],
+  imports: [
+    LoginComponent,
     NgIf,
     MatFormFieldModule,
     MatInputModule,
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,8 +5,10 @@ import {MatInputModule} from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup,ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
+
 
 
 @Component({
@@ -15,11 +17,12 @@ import { Router } from '@angular/router';
   imports: [
     NgIf,
     MatFormFieldModule,
+    ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
 
@@ -27,9 +30,12 @@ export class LoginComponent {
   loginForm!: FormGroup;
 
   constructor(
+
+    //private authService: AuthService,
     private formBuilder: FormBuilder,
     private router: Router,
     private snackBar: MatSnackBar
+    
   ){}
 
   ngOnInit(): void {
@@ -38,5 +44,35 @@ export class LoginComponent {
       password: ['', [Validators.required, Validators.minLength(2)]]
     });
   }
+  onSubmit() {
+    /*if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')!.value;
+      const password = this.loginForm.get('password')!.value;
+      this.authService.login(email, password).subscribe({
+        next: (resp) => {
+          // redirecionar para a página principal
+          this.router.navigateByUrl('/home');
+        },
+        error: (err) => {
+          console.log(err);
+          this.showSnackbarTopPosition("Usuário ou senha Inválidos", 'Fechar', 2000);
+        }
+      });
+    } else {
+      this.showSnackbarTopPosition("Dados inválidos", 'Fechar', 2000);
+    }*/
+  }
+
+  /*onRegister() {
+    // criar usuário
+  }
+
+  showSnackbarTopPosition(content: any, action: any, duration: any) {
+    this.snackBar.open(content, action, {
+      duration: 2000,
+      verticalPosition: "top", // Allowed values are  'top' | 'bottom'
+      horizontalPosition: "center" // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
+    });
+  }*/
 
 }
